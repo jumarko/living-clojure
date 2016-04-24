305742cc41a04969eed1f07fa2ba62d4065f8c9f
Juraj Martinka <juraj@snapshot.travel>
2016-04-24 15:23:55 +0200

    Chapter 7: HTML templating with Enfocus library and ClojureScript

--- cheshire-cat/project.clj
+++ cheshire-cat/project.clj
@@ -5,9 +5,21 @@
   :dependencies [[org.clojure/clojure "1.7.0"]
                  [compojure "1.4.0"]
                  [ring/ring-defaults "0.1.5"]
-                 [ring/ring-json "0.4.0"]]
-  :plugins [[lein-ring "0.9.7"]]
+                 [ring/ring-json "0.4.0"]
+                 [org.clojure/clojurescript "1.8.40"]
+                 [cljs-http "0.1.40"]
+                 [org.clojure/core.async "0.2.374"]
+                 [enfocus "2.1.1"]]
+  :plugins [[lein-ring "0.9.7"]
+            [lein-cljsbuild "1.1.3"]]
   :ring {:handler cheshire-cat.handler/app}
   :profiles
   {:dev {:dependencies [[javax.servlet/servlet-api "2.5"]
-                        [ring/ring-mock "0.3.0"]]}})
+                        [ring/ring-mock "0.3.0"]]}}
+  :cljsbuild {
+              :builds [{
+                        :source-paths ["src-cljs"]
+                        :compiler {
+                                   :output-to "resources/public/main.js"
+                                   :optimizations :whitespace
+                                   :pretty-print true}}]})
