77ec75ba6ccc43262afbe81c29c4467bc010c6df
Juraj Martinka <juraj@snapshot.travel>
2016-04-24 12:27:03 +0200

    Chapter 7: automatic conversion of responses to the JSON via ring-json middleware

--- cheshire-cat/project.clj
+++ cheshire-cat/project.clj
@@ -4,10 +4,10 @@
   :min-lein-version "2.0.0"
   :dependencies [[org.clojure/clojure "1.7.0"]
                  [compojure "1.4.0"]
-                 [ring/ring-defaults "0.1.5"]]
+                 [ring/ring-defaults "0.1.5"]
+                 [ring/ring-json "0.4.0"]]
   :plugins [[lein-ring "0.9.7"]]
   :ring {:handler cheshire-cat.handler/app}
   :profiles
   {:dev {:dependencies [[javax.servlet/servlet-api "2.5"]
-                        [ring/ring-mock "0.3.0"]
-                        [cheshire "5.6.1"]]}})
+                        [ring/ring-mock "0.3.0"]]}})
